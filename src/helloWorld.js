fff91461002177162bdb5f13fcfba333a1c77c42
itachi-P <itachip38@gmail.com>
2023-07-30 12:05:52 +0900

    update: Modified code according to ESLint standards

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -4,5 +4,10 @@
 //3.console.logを残している
 //4.セミコロンを省略している
 
-export const hello_world = "Hello World";
-console.log(hello_world)
+//自動修正を実行し、エラーのうち2.と4.が修正されるのを確認
+// npx eslint --fix src/
+//手動で変数名をcamelCaseに修正し、console.logを削除
+// 最後に、npx eslint src/を実行し、エラーが消えていることを確認
+
+export const helloWorld = 'Hello World';
+//console.log(hello_world); //コメントアウトでもエラーは消える
