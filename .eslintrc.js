d26d0526aa16b1dec2f2a1636c2809be36335f3f
itachi-P <itachip38@gmail.com>
2023-07-30 11:27:33 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // console.log()をコードに残しておいてはいけない
+    "no-console": "error",
+    // プロパティ名に限ってはキャメルケースを強制しない(_を許容する)
+    "camelcase": ["error", { properties: "never" }],
+    // セミコロンを強制する(文末のセミコロンを省略できない)
+    "semi": ["error", "always"],
+    // 文字列はシングルクォーテーションで囲む（エラーにはならないが警告を出す）
+    "quotes": ["warn", 'single'],
+  },
+};
---
+++ src/helloWorld.js
@@ -0,0 +1,8 @@
+//わざとエラー(及び警告)を出す
+//1.変数名がcamelCaseでない
+//2.文字列がシングルクォーテーションで囲まれていない(警告)
+//3.console.logを残している
+//4.セミコロンを省略している
+
+export const hello_world = "Hello World";
+console.log(hello_world)
