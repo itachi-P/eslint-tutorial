5c04fcbf5f82bebac47b79f1afe16d7348e20570
itachi-P <itachip38@gmail.com>
2023-07-30 14:06:27 +0900

    Implement a shareable configuration (Airbnb-base).

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,27 @@ module.exports = {
     es2021: true,
   },
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  // rules: {
+  //   // console.log()をコードに残しておいてはいけない
+  //   'no-console': 'error',
+  //   // プロパティ名に限ってはキャメルケースを強制しない(_を許容する)
+  //   'camelcase': ['error', { properties: 'never' }],
+  //   // セミコロンを強制する(文末のセミコロンを省略できない)
+  //   'semi': ['error', 'always'],
+  //   // 文字列はシングルクォーテーションで囲む（エラーにはならないが警告を出す）
+  //   'quotes': ['warn', 'single'],
+  // },
+
+  // rulesの代わりに有名なShareable config(ルールのプリセット)を導入する
+  extends: ["airbnb-base"],
+  // Shareable configのルールを上書きする場合はrulesを使う
   rules: {
-    // console.log()をコードに残しておいてはいけない
-    'no-console': 'error',
-    // プロパティ名に限ってはキャメルケースを強制しない(_を許容する)
-    'camelcase': ['error', { properties: 'never' }],
-    // セミコロンを強制する(文末のセミコロンを省略できない)
-    'semi': ['error', 'always'],
-    // 文字列はシングルクォーテーションで囲む（エラーにはならないが警告を出す）
-    'quotes': ['warn', 'single'],
+    // デフォルトimportを強制せず、名前付きimportを許容する
+    "import/prefer-default-export": "off",
+    // 文字列リテラルはダブルクォートのほうを使いたい
+    quotes: ["error", "double"],
   },
 };
--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,13 +1,5 @@
-//わざとエラー(及び警告)を出す
-//1.変数名がcamelCaseでない
-//2.文字列がシングルクォーテーションで囲まれていない(警告)
-//3.console.logを残している
-//4.セミコロンを省略している
+// rulesを個別に設定するのでなく、有名なAirbnbのShareable configを導入する
+// わざとAirbnbの規約に違反するコードを書いてみる
 
-//自動修正を実行し、エラーのうち2.と4.が修正されるのを確認
-// npx eslint --fix src/
-//手動で変数名をcamelCaseに修正し、console.logを削除
-// 最後に、npx eslint src/を実行し、エラーが消えていることを確認
-
-export const helloWorld = 'Hello World';
-//console.log(hello_world); //コメントアウトでもエラーは消える
+export const helloWorld = "Hello World";
+// console.log(helloWorld);
